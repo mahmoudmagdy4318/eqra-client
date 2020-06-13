2cb9c0be50f0ebd2325c426226b1a22f06785cf3
eslam <eslamelkholy5325762@gmail.com>
2020-06-13 20:02:24 +0200

    Add Create new Event Button for Writers Only

--- src/Event/EventNavbar.jsx
+++ src/Event/EventNavbar.jsx
@@ -1,72 +1,74 @@
-import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import Popover from '@material-ui/core/Popover';
-import MenuList from '@material-ui/core/MenuList';
-import Paper from '@material-ui/core/Paper';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import HomeIcon from '@material-ui/icons/Home';
-import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
-import MessageIcon from '@material-ui/icons/Message';
-import PersonIcon from '@material-ui/icons/Person';
-import SendIcon from '@material-ui/icons/Send';
-import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
+import React from "react";
+import { UserContext } from "../context/userContext";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import Popover from "@material-ui/core/Popover";
+import MenuList from "@material-ui/core/MenuList";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import HomeIcon from "@material-ui/icons/Home";
+import MessageIcon from "@material-ui/icons/Message";
+import PersonIcon from "@material-ui/icons/Person";
+import { Button } from "@material-ui/core";
 const EventNavBar = (props) => {
-    const classes = useStyles();
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const [anchorEl2, setAnchorEl2] = React.useState(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const {
+    data: { user: currentUser },
+  } = React.useContext(UserContext);
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl2, setAnchorEl2] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-    const handleProfileMenuOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleMobileMenuClose = () => {
-        setMobileMoreAnchorEl(null);
-    };
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
 
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-        handleMobileMenuClose();
-    };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
 
-    const handleMobileMenuOpen = (event) => {
-        setMobileMoreAnchorEl(event.currentTarget);
-    };
-    // Right Drop Downmenu Functionality
-    const handleClick = (event) => {
-        setAnchorEl2(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl2(null);
-    }
-    const open = Boolean(anchorEl2);
-    const id = open ? 'simple-popover' : undefined;
-    // ========== End Right DropDown Menu ============
-  const menuId = 'primary-search-account-menu';
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+  // Right Drop Downmenu Functionality
+  const handleClick = (event) => {
+    setAnchorEl2(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl2(null);
+  };
+  const open = Boolean(anchorEl2);
+  const id = open ? "simple-popover" : undefined;
+  // ========== End Right DropDown Menu ============
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={menuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
@@ -74,14 +76,14 @@ const EventNavBar = (props) => {
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={mobileMenuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
@@ -119,7 +121,7 @@ const EventNavBar = (props) => {
     <div className={classes.grow}>
       <AppBar position="static">
         <Toolbar>
-        {/* Right Dropdown Menu Button & Menu*/}
+          {/* Right Dropdown Menu Button & Menu*/}
           <IconButton
             edge="start"
             className={classes.menuButton}
@@ -130,46 +132,46 @@ const EventNavBar = (props) => {
             <MenuIcon />
           </IconButton>
           <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl2}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-      >
-      <MenuList>
-        <MenuItem>
-          <ListItemIcon>
-            <HomeIcon fontSize="medium" color={"primary"} />
-          </ListItemIcon>
-          <Typography variant="inherit">Home</Typography>
-        </MenuItem>
-        <MenuItem>
-          <ListItemIcon>
-            <PersonIcon fontSize="medium" color={"primary"} />
-          </ListItemIcon>
-          <Typography variant="inherit">Profile</Typography>
-        </MenuItem>
-        <MenuItem>
-          <ListItemIcon>
-            <NotificationsIcon fontSize="medium" color={"primary"}/>
-          </ListItemIcon>
-          <Typography variant="inherit">Notifications</Typography>
-        </MenuItem>
-        <MenuItem>
-          <ListItemIcon>
-            <MessageIcon fontSize="medium" color={"primary"} />
-          </ListItemIcon>
-          <Typography variant="inherit">Messages</Typography>
-        </MenuItem>
-      </MenuList>
-      </Popover>
+            id={id}
+            open={open}
+            anchorEl={anchorEl2}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <MenuList>
+              <MenuItem>
+                <ListItemIcon>
+                  <HomeIcon fontSize="medium" color={"primary"} />
+                </ListItemIcon>
+                <Typography variant="inherit">Home</Typography>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <PersonIcon fontSize="medium" color={"primary"} />
+                </ListItemIcon>
+                <Typography variant="inherit">Profile</Typography>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <NotificationsIcon fontSize="medium" color={"primary"} />
+                </ListItemIcon>
+                <Typography variant="inherit">Notifications</Typography>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <MessageIcon fontSize="medium" color={"primary"} />
+                </ListItemIcon>
+                <Typography variant="inherit">Messages</Typography>
+              </MenuItem>
+            </MenuList>
+          </Popover>
           <Typography className={classes.title} variant="h6" noWrap>
             Eqraa Online
           </Typography>
@@ -183,11 +185,26 @@ const EventNavBar = (props) => {
                 root: classes.inputRoot,
                 input: classes.inputInput,
               }}
-              inputProps={{ 'aria-label': 'search' }}
+              inputProps={{ "aria-label": "search" }}
             />
           </div>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
+            {currentUser.role === "writer" ? (
+              <Button 
+              href="/newEvent"
+                variant="contained"
+                color="primary"
+                size="small"
+                className={classes.button}
+                startIcon={<EventNoteIcon />}
+              >
+                Create Event
+              </Button>
+            ) : (
+              ""
+            )}
+
             <IconButton aria-label="show 4 new mails" color="inherit">
               <Badge badgeContent={4} color="secondary">
                 <MailIcon />
@@ -226,74 +243,72 @@ const EventNavBar = (props) => {
       {renderMenu}
     </div>
   );
-}
+};
 export default EventNavBar;
 
 const useStyles = makeStyles((theme) => ({
-    grow: {
-      flexGrow: 1,
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
-    title: {
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
-      },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
     },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-      },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
     },
-    inputRoot: {
-      color: 'inherit',
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
     },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: '20ch',
-      },
-    },
-    sectionDesktop: {
-      display: 'none',
-      [theme.breakpoints.up('md')]: {
-        display: 'flex',
-      },
-    },
-    sectionMobile: {
-      display: 'flex',
-      [theme.breakpoints.up('md')]: {
-        display: 'none',
-      },
-      
-    },
-    typography: {
-        padding: theme.spacing(2),
-      }
-  }));
-  
+  },
+  typography: {
+    padding: theme.spacing(2),
+  },
+}));
