bb622cd37be7a5f96f1db56fda27fc5e8dcbb339
Mahmoud <mahmoedmohamed19@gmail.com>
2020-06-18 16:06:35 +0200

    refactoring writer posts

--- src/Layout/Profile/writerComponents/Posts/Post.jsx
+++ src/Layout/Profile/writerComponents/Posts/Post.jsx
@@ -1,24 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Checkbox, FormControlLabel, TextField, Card, Typography, CardContent, CardActions, Menu, MenuItem, Paper, IconButton, InputBase, Divider } from '@material-ui/core';
-import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import SendIcon from '@material-ui/icons/Send';
+import { Button, Checkbox, FormControlLabel, TextField } from '@material-ui/core';
+
 
 import styles from './post.module.css';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import axiosInstance from '../../../../API/axiosInstance';
-import CustomizedDialogs from '../../../../utils/Edit';
+import SinglePost from './SinglePost';
 
 const Post = ({ name, image, setNewFeaturedPosts, userid }) => {
   let [featured, setFeatured] = useState(false);
   let [postBody, setPostBody] = useState('');
   let [postList, setPostList] = useState([]);
-  let [postData, setPostData] = useState({});
-  let [inputField, setInputField] = useState(false)
-  let [postId, setPostId] = useState(0);
-  let [EditPopUp, setEditPopUp] = useState(false);
   let [currentUserLikes, setCurrentUserLikes] = useState([]);
 
   let [currPage, setCurrPage] = useState(1);
@@ -43,27 +35,6 @@ const Post = ({ name, image, setNewFeaturedPosts, userid }) => {
     }
   };
 
-  const deletePost = (id) => async () => {
-    try {
-      await axiosInstance.delete(`api/post/${id}`);
-      setAnchorEl(null);
-      setPostList(postList.filter(p => p.id !== id));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-
-  const EditingPost = (postData) => {
-    console.log(postData);
-    return axiosInstance
-      .patch(`/api/post/${postData.id}`, postData)
-      .then((res) => {
-        console.log(res);
-        setPostList([...postList.map((p) => (p.id === postData.id ? postData : p))]);
-      });
-  };
-
   useEffect(() => {
     axiosInstance.get(`api/userposts/${userid}?page=${currPage}`)
       .then((data) => {
@@ -81,35 +52,8 @@ const Post = ({ name, image, setNewFeaturedPosts, userid }) => {
     )
   }, [userid])
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = (event, post) => {
-    setPostData(post);
-    setPostId(post.id);
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const reactLove = (id) => {
-    if (currentUserLikes.includes(id)) {
-      return axiosInstance
-        .delete(`api/post/${id}/likes/${userid}`)
-        .then(() => {
-          setCurrentUserLikes(currentUserLikes.filter(item => item !== id));
-        });
-    } else {
-      return axiosInstance
-        .post("api/post/like", {
-          post_id: id,
-          user_id: userid,
-        })
-        .then(() => {
-          setCurrentUserLikes([...currentUserLikes, id]);
-        });
-    }
+  const deletedPost = (id) => {
+    setPostList(postList.filter(p => p.id !== id));
   }
 
   return (
@@ -160,67 +104,21 @@ const Post = ({ name, image, setNewFeaturedPosts, userid }) => {
         >
           {postList.map((post) => {
             return (
-              <Card id={`post-${post.id}`} className={`${styles.root} ${post.new ? styles.new_added : ""}`} variant="outlined" key={post.id}>
-                <Button className={styles.options} aria-controls="simple-menu" aria-haspopup="true" onClick={(e) => handleClick(e, post)}>
-                  <ExpandMoreIcon />
-                </Button>
-                <Menu
-                  id={`post-${post.id}`}
-                  anchorEl={anchorEl}
-                  keepMounted
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-                >
-                  <MenuItem onClick={deletePost(postId)}>Delete</MenuItem>
-                  <MenuItem onClick={() => { setEditPopUp(true); }}>Edit</MenuItem>
-                </Menu>
-                <CardContent className={styles.posts_showPosts_body} >
-                  <div className={styles.posts_showPosts_body_title} >
-                    <img src={image} alt="" />
-                    <Typography color="textSecondary" className={styles.posts_showPosts_body_title_username} gutterBottom>
-                      {name}
-                    </Typography>
-                  </div>
-                  <Typography variant="body2" component="p" style={{ padding: '2px 30px' }}>
-                    {post.body_content}
-                  </Typography>
-                </CardContent>
-                <CardActions className={styles.post_action} >
-                  <ChatBubbleOutlineIcon className={styles.post_action_icons} onClick={() => setInputField(true)} />
-                  <div>
-                    {currentUserLikes.includes(post.id) ?
-                      <FavoriteIcon color={'secondary'} className={styles.post_action_icons} onClick={() => reactLove(post.id)} /> :
-                      <FavoriteBorderIcon className={styles.post_action_icons} onClick={() => reactLove(post.id)} />
-                    }
-                    {post.likes}
-                  </div>
-                </CardActions>
-
-                <Paper component="form" className={styles.paperRoot} onSubmit={(e)=>{e.preventDefault();console.log(e)}}>
-                  <InputBase
-                    className={styles.input}
-                    placeholder="Search Google Maps"
-                    inputProps={{ 'aria-label': 'search google maps' }}
-                  />
-                  <Divider className={styles.divider} orientation="vertical" />
-                  <IconButton color="primary" className={styles.iconButton} aria-label="directions">
-                    <SendIcon />
-                  </IconButton>
-                </Paper>
-
-              </Card>
+              <SinglePost
+                post={post}
+                currentUserLikes={currentUserLikes}
+                deletedPost={deletedPost}
+                userid={userid}
+                image={image}
+                name={name}
+                setCurrentUserLikes={setCurrentUserLikes}
+                setPostList={setPostList}
+                postList={postList}
+              />
             )
           })}
         </InfiniteScroll>
-
-
       </div>
-      <CustomizedDialogs
-        open={EditPopUp}
-        setOpen={setEditPopUp}
-        post={postData}
-        submitEditingPost={EditingPost}
-      />
     </section >
   );
 };
--- src/Layout/Profile/writerComponents/Posts/SinglePost.jsx
+++ src/Layout/Profile/writerComponents/Posts/SinglePost.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { Button, Card, Typography, CardContent, CardActions, Menu, MenuItem, Paper, IconButton, InputBase, Divider } from '@material-ui/core';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
@@ -7,24 +7,38 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import SendIcon from '@material-ui/icons/Send';
 
 import styles from './post.module.css';
+import CustomizedDialogs from '../../../../utils/Edit';
+import axiosInstance from '../../../../API/axiosInstance';
+import { Link } from 'react-router-dom';
 
-const SinglePost = ({post,setPostData}) => {
-
+const SinglePost = ({ post, deletedPost, currentUserLikes, userid, image, name, setCurrentUserLikes, setPostList, postList }) => {
+  let [postData, setPostData] = useState({});
   let [inputField, setInputField] = useState(false)
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  let [anchorEl, setAnchorEl] = React.useState(null);
+  let [EditPopUp, setEditPopUp] = useState(false);
+  let [commentBody, setCommentBody] = useState("")
+
+  const EditingPost = (postData) => {
+    console.log(postData);
+    return axiosInstance
+      .patch(`/api/post/${postData.id}`, postData)
+      .then((res) => {
+        console.log(res);
+        setPostList([...postList.map((p) => (p.id === postData.id ? postData : p))]);
+      });
+  };
   const deletePost = (id) => async () => {
     try {
       await axiosInstance.delete(`api/post/${id}`);
       setAnchorEl(null);
-      setPostList(postList.filter(p => p.id !== id));
+      deletedPost(post.id)
     } catch (error) {
       console.log(error);
     }
   };
-  
+
   const handleClick = (event, post) => {
     setPostData(post);
-    setPostId(post.id);
     setAnchorEl(event.currentTarget);
   };
 
@@ -51,56 +65,75 @@ const SinglePost = ({post,setPostData}) => {
     }
   }
 
+  const newComment = (e, id) => {
+    e.preventDefault();
+    console.log(commentBody);
+    setCommentBody("");
+    setInputField(false);
+    axiosInstance.post("api/comment", { content: commentBody, postId: id }).then(() => { setInputField(false) });
+
+  }
+
   return (
-    <Card id={`post-${post.id}`} className={`${styles.root} ${post.new ? styles.new_added : ""}`} variant="outlined" key={post.id}>
-      <Button className={styles.options} aria-controls="simple-menu" aria-haspopup="true" onClick={(e) => handleClick(e, post)}>
-        <ExpandMoreIcon />
-      </Button>
-      <Menu
-        id={`post-${post.id}`}
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={deletePost(postId)}>Delete</MenuItem>
-        <MenuItem onClick={() => { setEditPopUp(true); }}>Edit</MenuItem>
-      </Menu>
-      <CardContent className={styles.posts_showPosts_body} >
-        <div className={styles.posts_showPosts_body_title} >
-          <img src={image} alt="" />
-          <Typography color="textSecondary" className={styles.posts_showPosts_body_title_username} gutterBottom>
-            {name}
+    <>
+      <Card id={`post-${post.id}`} className={`${styles.root} ${post.new ? styles.new_added : ""}`} variant="outlined" key={post.id}>
+        <Button className={styles.options} aria-controls="simple-menu" aria-haspopup="true" onClick={(e) => handleClick(e, post)}>
+          <ExpandMoreIcon />
+        </Button>
+        <Menu
+          id={`post-${post.id}`}
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={deletePost(post.id)}>Delete</MenuItem>
+          <MenuItem onClick={() => { setEditPopUp(true); }}>Edit</MenuItem>
+        </Menu>
+        <CardContent className={styles.posts_showPosts_body} >
+          <div className={styles.posts_showPosts_body_title} >
+            <img src={image} alt="" />
+            <Typography color="textSecondary" className={styles.posts_showPosts_body_title_username} gutterBottom>
+              {name}
+            </Typography>
+          </div>
+          <Typography variant="body2" component="p" style={{ padding: '2px 30px' }}>
+            <Link to={`/post/${post.id}`} >{post.body_content} </Link>
           </Typography>
-        </div>
-        <Typography variant="body2" component="p" style={{ padding: '2px 30px' }}>
-          {post.body_content}
-        </Typography>
-      </CardContent>
-      <CardActions className={styles.post_action} >
-        <ChatBubbleOutlineIcon className={styles.post_action_icons} onClick={() => setInputField(true)} />
-        <div>
-          {currentUserLikes.includes(post.id) ?
-            <FavoriteIcon color={'secondary'} className={styles.post_action_icons} onClick={() => reactLove(post.id)} /> :
-            <FavoriteBorderIcon className={styles.post_action_icons} onClick={() => reactLove(post.id)} />
-          }
-          {post.likes}
-        </div>
-      </CardActions>
+        </CardContent>
+        <CardActions className={styles.post_action} >
+          <ChatBubbleOutlineIcon className={styles.post_action_icons} onClick={() => setInputField(!inputField)} />
+          <div>
+            {currentUserLikes.includes(post.id) ?
+              <FavoriteIcon color={'secondary'} className={styles.post_action_icons} onClick={() => reactLove(post.id)} /> :
+              <FavoriteBorderIcon className={styles.post_action_icons} onClick={() => reactLove(post.id)} />
+            }
+            {post.likes}
+          </div>
+        </CardActions>
 
-      <Paper component="form" className={styles.paperRoot} onSubmit={(e) => { e.preventDefault(); console.log(e) }}>
-        <InputBase
-          className={styles.input}
-          placeholder="Search Google Maps"
-          inputProps={{ 'aria-label': 'search google maps' }}
-        />
-        <Divider className={styles.divider} orientation="vertical" />
-        <IconButton color="primary" className={styles.iconButton} aria-label="directions">
-          <SendIcon />
-        </IconButton>
-      </Paper>
+        <Paper component="form" className={`${styles.paperRoot} ${inputField ? styles.shown : ""}`} onSubmit={(e) => newComment(e, post.id)}>
+          <InputBase
+            className={styles.input}
+            placeholder="Search Google Maps"
+            inputProps={{ 'aria-label': 'search google maps' }}
+            value={commentBody}
+            onChange={(e) => { setCommentBody(e.target.value) }}
+          />
+          <Divider className={styles.divider} orientation="vertical" />
+          <IconButton color="primary" className={styles.iconButton} aria-label="directions" onClick={(e) => newComment(e, post.id)}>
+            <SendIcon />
+          </IconButton>
+        </Paper>
 
-    </Card>
+      </Card>
+      <CustomizedDialogs
+        open={EditPopUp}
+        setOpen={setEditPopUp}
+        post={postData}
+        submitEditingPost={EditingPost}
+      />
+    </>
   )
 }
 
