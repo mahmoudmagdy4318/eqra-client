65f76b2238d88278d5ed4034d602ebfcbcff7807
Mahmoud <mahmoedmohamed19@gmail.com>
2020-06-19 17:02:15 +0200

    handling books

--- src/Layout/Profile/writerComponents/books/Book.jsx
+++ src/Layout/Profile/writerComponents/books/Book.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
-import { Button, Card, Typography, CardContent, CardActions, makeStyles } from '@material-ui/core';
+import React, { useState, useEffect } from 'react'
+import { Button, Card, Typography, CardContent, CardActions, makeStyles, InputLabel, OutlinedInput, InputAdornment, FormControl } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-
+import axiosInstance from "../../../../API/axiosInstance";
 import styles from './book.module.css'
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -19,16 +25,36 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
+  Modalpaper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
-
 const Book = () => {
-  let [books, setBooks] = useState([
-    { id: 1, title: 'the arsonist', img: 'https://www.booktopia.com.au/blog/wp-content/uploads/2018/12/the-arsonist.jpg' },
-    { id: 2, title: 'the water cube', img: 'https://s26162.pcdn.co/wp-content/uploads/2019/01/81SBy9jbbHL.jpg' },
-    { id: 3, title: 'the Institute', img: 'https://images-na.ssl-images-amazon.com/images/I/81pA6-hv+2L.jpg' },
-    { id: 34, title: 'the Institute', img: 'https://images-na.ssl-images-amazon.com/images/I/81pA6-hv+2L.jpg' },
-    { id: 5, title: 'the Institute', img: 'https://images-na.ssl-images-amazon.com/images/I/81pA6-hv+2L.jpg' },
-  ])
+  let [newBook, setNewBook] = useState({ title: '', description: '', price: 0, coverImage: null })
+
+  let [books, setBooks] = useState([])
+
+  const getBooks = async () => {
+    let booksData = await axiosInstance.get('api/user/books');
+    setBooks(booksData.userBooks);
+  }
+
+  useEffect(() => {
+    getBooks();
+  }, [])
 
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
@@ -41,12 +67,33 @@ const Book = () => {
     setOpen(false);
   };
 
+  const uploadBookCover = (e) => {
+    setNewBook({ ...newBook, coverImage: e.target.files[0] })
+  }
+
+  const submitNewBook = (e) => {
+    e.preventDefault();
+    if (newBook.coverImage.type.split('/')[0] !== "image")
+      alert('wrong file type')
+      
+    console.log(newBook);
+    
+    axiosInstance.interceptors.request.use((cfg) => {
+      cfg.headers["Contetnt-Type"]="multipart/form-data";
+      return cfg;
+    })
+    axiosInstance.post('/api/book', newBook)
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+
+
+  }
   return (
     <>
       <div className={styles.newBook}>
         <button type="button" onClick={handleOpen}>
-          react-transition-group
-      </button>
+          Add New Book
+        </button>
         <Modal
           aria-labelledby="transition-modal-title"
           aria-describedby="transition-modal-description"
@@ -61,14 +108,96 @@ const Book = () => {
         >
           <Fade in={open}>
             <div className={classes.paper}>
-              <h2 id="transition-modal-title">Transition modal</h2>
-              <p id="transition-modal-description">react-transition-group animates me.</p>
+              <h2 id="transition-modal-title">New Book</h2>
+              <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.Modalpaper}>
+                  <form className={classes.form} validate="true" onSubmit={submitNewBook}>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12} md={6}>
+                        <TextField
+                          autoComplete="fname"
+                          name="title"
+                          variant="outlined"
+                          required
+                          fullWidth
+                          value={newBook.title}
+                          onChange={(e) => setNewBook({ ...newBook, title: e.target.value })}
+                          id="title"
+                          label="Title"
+                          autoFocus
+                        />
+                      </Grid>
+                      <Grid item xs={12} md={6}>
+                        <FormControl fullWidth className={classes.margin} variant="outlined">
+                          <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                          <OutlinedInput
+                            id="outlined-adornment-amount"
+                            name='price'
+                            value={newBook.price}
+                            onChange={(e) => setNewBook({ ...newBook, price: e.target.value })}
+                            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                            labelWidth={60}
+                            type='number'
+                            required
+                          />
+
+                        </FormControl>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          variant="outlined"
+                          required
+                          fullWidth
+                          multiline
+                          id="description"
+                          label="Description"
+                          name="description"
+                          value={newBook.description}
+                          onChange={(e) => setNewBook({ ...newBook, description: e.target.value })}
+                        />
+                      </Grid>
+
+                      <Grid item xs={12}>
+                        <Button
+                          variant="outlined"
+                          component="label"
+                          style={{ marginRight: 7 }}
+                        >
+                          Upload Cover Image
+                        <input
+                            name="coverImage"
+                            onChange={uploadBookCover}
+                            required
+                            type="file"
+                            style={{ display: "none" }}
+                          />
+                        </Button>
+                        {newBook.coverImage !== null && newBook.coverImage.type.split('/')[0] === "image" ?
+                          <CheckCircleOutlineIcon style={{ color: '#4caf50' }} /> :
+                          <ErrorOutlineIcon style={{ color: '#f44336' }} />
+                        }
+                      </Grid>
+
+                    </Grid>
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      className={classes.submit}
+                    >
+                      Add New Book
+                    </Button>
+                  </form>
+                </div>
+              </Container>
             </div>
           </Fade>
         </Modal>
       </div>
       <div className={styles.grid}>
-        {books.slice(0).reverse().map((book) => {
+        {books.length === 0 ? 'no books found' : books.slice(0).reverse().map((book) => {
           return (
             <Card className={styles.item} variant="outlined" key={book.id}>
               <CardContent >
